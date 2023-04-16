cb36f993087d727eceea97e0289d91d55405a5cd
Valerii Volkov <v.volkov@start.ru>
2023-04-16 17:11:22 +0400

    vsplit

--- lua/neo-tree/sources/ctags/commands.lua
+++ lua/neo-tree/sources/ctags/commands.lua
@@ -15,34 +15,39 @@ local function get_node_patterns(tree, node)
   return patterns
 end
 
-local M = {}
 
-M.open = function(state, toggle_directory)
-  local function prepare(pattern)
-    pattern = string.gsub(pattern, "%[", "\\[")
-    pattern = string.gsub(pattern, "%]", "\\]")
-    return pattern
-  end
-
-  local tree = state.tree
-
-  local node = tree:get_node()
-  if node.type == "file" then
-    local extra = node.extra or {}
-    vim.cmd("e +0 " .. extra.filename)
-    local patterns = get_node_patterns(tree, node)
-    for _, pattern in ipairs(patterns) do
-      if pattern ~= nil then vim.cmd("silent! " .. prepare(pattern)) end
+local function open_some(create_buffer)
+  return function(state, toggle_directory)
+    local function prepare(pattern)
+      pattern = string.gsub(pattern, "%[", "\\[")
+      pattern = string.gsub(pattern, "%]", "\\]")
+      return pattern
     end
-    vim.cmd("nohlsearch")
-    return
-  else
-    cc.open(state, toggle_directory)
+
+    local tree = state.tree
+
+    local node = tree:get_node()
+    if node.type == "file" then
+      local extra = node.extra or {}
+      create_buffer(extra.filename)
+      local patterns = get_node_patterns(tree, node)
+      for _, pattern in ipairs(patterns) do
+        if pattern ~= nil then vim.cmd("silent! " .. prepare(pattern)) end
+      end
+      vim.cmd("nohlsearch")
+      return
+    else
+      cc.open(state, toggle_directory)
+    end
   end
 end
 
-M.refresh = function(state)
-end
+local M = {}
+M.open = open_some(function(filename) vim.cmd("edit +0 " .. filename) end)
+M.open_split = open_some(function(filename) vim.cmd("vsplit +0 " .. filename) end)
+M.open_split_window_picker = function(state, toggle_directory) end
+M.open_tabnew = open_some(function(filename) vim.cmd("tabnew +0 " .. filename) end)
+M.refresh = function(state) end
 --
 cc._add_common_commands(M)
 
